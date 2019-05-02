88e809b32609ae5f476af432beb268a4380d666d
beautifulrockss@gmail.com <nikhilmadey@192.168.0.101>
2019-05-02 07:36:52 +0530

    1st commit

---
+++ eclipse-workspace/sample3/src/main/java/sample3/SumDemo.java
@@ -0,0 +1,21 @@
+package sample3;
+
+import java.util.Scanner;
+class SumDemo{
+   private static Scanner scanner;
+
+public static void main(String args[]){
+      scanner = new Scanner(System.in);
+      int[] array = new int[10];
+      int sum = 0;
+      System.out.println("Enter the elements:");
+      for (int i=0; i<10; i++)
+      {
+    	  array[i] = scanner.nextInt();
+      }
+      for( int num : array) {
+          sum = sum+num;
+      }
+      System.out.println("Sum of array elements is:"+sum);
+   }
+}
---
+++ eclipse-workspace/sample3/src/test/java/sample3/sumdemotest.java
@@ -0,0 +1,14 @@
+package sample3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sumdemotest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
